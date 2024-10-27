b0c41ee25376c7c33b9333af576e869b5d681d3e
Arif Dhali <arifdhali98@gmail.com>
2024-10-27 22:29:55 +0530

    vite custom server remove

--- app/web/vite.config.js
+++ app/web/vite.config.js
@@ -7,20 +7,8 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 3000
-  },
-  build: {
-    rollupOptions: {
-      // Add configuration for single-page app fallback
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
-  // Configure for SPA fallback
-  optimizeDeps: {
-    exclude: ['vite-plugin-html'],
-  },
+  // server: {
+  //   host: process.env.HOST || 'localhost',
+  //   port:process.env.PORT || 3000
+  // }
 });
