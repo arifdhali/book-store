88e73f5816d4f67c45a24f2ec1919f4593a47e2c
Arif Dhali <arifdhali98@gmail.com>
2024-10-27 22:43:27 +0530

    add vite server

--- app/web/vite.config.js
+++ app/web/vite.config.js
@@ -9,18 +9,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: process.env.HOST || 'localhost',
-    port: process.env.PORT || 3000
-  },
-  build: {
-    rollupOptions: {
-      // Add configuration for single-page app fallback
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
-  // Configure for SPA fallback
-  optimizeDeps: {
-    exclude: ['vite-plugin-html'],
-  },
+    port:process.env.PORT || 3000
+  }
 });
