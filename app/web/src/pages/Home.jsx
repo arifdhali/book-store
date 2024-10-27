8b1b89fdda5f2eafafd7c38cdac3961b57ba8855
Arif Dhali <arifdhali98@gmail.com>
2024-10-28 00:26:56 +0530

    home admin add

--- app/web/src/pages/Home.jsx
+++ app/web/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import AppUrl from "../routes/routes";
 
 const Home = () => {
     return (
         <div>
             <h1>This is index pages for users / admin  / author</h1>
+            <Link to={AppUrl.ADMIN.BASE}>Admin</Link>
         </div>
     )
 }
