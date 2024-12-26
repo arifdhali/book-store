d284f951374ce5619fc0956269c59c06100e1f50
Arif Dhali <arifdhali98@gmail.com>
2024-12-26 22:55:46 +0530

    subsctipn page and add maxium quantity

--- app/web/src/pages/author/Mysubscription.jsx
+++ app/web/src/pages/author/Mysubscription.jsx
@@ -1,44 +1,85 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCrown, faCalendar, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCrown, faCalendar, faSyncAlt, faCheck, faClose } from '@fortawesome/free-solid-svg-icons';
 
 const MySubscription = () => {
     return (
-        <section className=" p-4 bg-white rounded-2">
+        <section className="subscription p-4 bg-light rounded-2">
             <div className="row">
-                <div className="col-md-3 ">
-                    <div className="rounded-2 border-1 border-light panel basic-plan">
-                        <div className="p-2 px-4 panel-heading ">
-                            <h3>Basic</h3>
+                {/* Basic Plan */}
+                <div className="col-md-4">
+                    <div className="shadow rounded-2 border border-2 border-info active-plan basic-plan">
+                        <div className="p-3 text-center bg-info text-white">
+                            <h3>Basic Plan</h3>
+                            <p className="mb-0"><strong>$0</strong>/month</p>
                         </div>
-                        <div className="panel-body p-4 border-1">
-                            <h5>Features Include</h5>
-                            <ul>
-                                <li>
-                                    Up to 2 users
-                                </li>
-                                <li>
-                                    Access to 2 service workflows
-                                </li>
-                                <li>Basic analytics reporting
-                                </li>
-                                <li>Unlimited Invoice</li>
+                        <div className="p-4">
+                            <h5 className="text-info">Features</h5>
+                            <ul className="list-unstyled">
+                                <li><FontAwesomeIcon icon={faCheck} /> Up to <strong>10</strong> Books</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Basic Analytics</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: 10 Quantity</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: 30% margin</li>
+                                <li><FontAwesomeIcon icon={faClose} /> Advanced Analytics</li>
                             </ul>
                         </div>
-                        <div className="panel-footer p-4">
-                            <div className="pricingButton">
-
-                                <a
-                                    href="register?plan=basic_monthly"
-                                    className="btn btn-primary"
-                                >
-                                    Get Started
-                                </a>
-                            </div>
+                        <div className="p-3 text-center">
+                            <button className="btn btn-info w-100" disabled>
+                                Current Plan
+                            </button>
                         </div>
                     </div>
                 </div>
 
+                {/* Standard Plan */}
+                <div className="col-md-4">
+                    <div className="shadow rounded-2 border  standard-plan">
+                        <div className="p-3 text-center bg-secondary text-white">
+                            <h3>Standard Plan</h3>
+                            <p className="mb-0"><strong>$15</strong>/month</p>
+                        </div>
+                        <div className="p-4">
+                            <h5 className="text-secondary">Features</h5>
+                            <ul className="list-unstyled">
+                                <li><FontAwesomeIcon icon={faCheck} /> Up to <strong>30</strong> Books</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Basic Analytics</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: 40 Quantity</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: 10% margin</li>
+                                <li><FontAwesomeIcon icon={faClose} /> Advanced Analytics</li>
+                            </ul>
+                        </div>
+                        <div className="p-3 text-center">
+                            <a href="register?plan=standard" className="btn btn-secondary w-100">
+                                Upgrade to Standard
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Premium Plan */}
+                <div className="col-md-4">
+                    <div className="shadow rounded-2 border  premium-plan">
+                        <div className="p-3 text-center bg-primary text-white">
+                            <h3>Premium Plan</h3>
+                            <p className="mb-0"><strong>$30</strong>/month</p>
+                        </div>
+                        <div className="p-4">
+                            <h5 className="text-primary">Features</h5>
+                            <ul className="list-unstyled">
+                                <li><FontAwesomeIcon icon={faCheck} /> Unlimited Books</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Advanced Analytics</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: Unlimited Quantity</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Per Book: 0% margin</li>
+                                <li><FontAwesomeIcon icon={faCheck} /> Comprehensive Analytics Reporting</li>
+                            </ul>
+                        </div>
+                        <div className="p-3 text-center">
+                            <a href="register?plan=premium" className="btn text-white bg-primary w-100">
+                                Upgrade to Premium
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
 
--- app/web/src/pages/author/book/Add.jsx
+++ app/web/src/pages/author/book/Add.jsx
@@ -34,6 +34,7 @@ const Add = () => {
       quantity: Yup.number()
         .required("Quantity is required")
         .min(1, "Quantity must be at least 1")
+        .max(10,"Maximum you can add 10 Quantity")
         .typeError("Quantity must be a number"),
       price: Yup.number().required("Price is required")
         .typeError("Price must be a number"),
