e4599559cef7d2a78139710f3f09e8dc22479708
Arif Dhali <arifdhali98@gmail.com>
2025-01-05 00:26:39 +0530

    folder create if doesn't exists for uploads

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,10 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require('fs');
 
 const multerStorgeHandle = (folderName) => {
+    let setFolderName = path.join("public", "uploads", folderName);
+    if (!fs.existsSync(setFolderName)) {
+        fs.mkdirSync(setFolderName, {
+            recursive: true,
+        })
+    }    
     const storage = multer.diskStorage({
         destination: function (req, res, cb) {
-            return cb(null, path.join(__dirname, `../public/uploads/${folderName}`));
+            return cb(null, setFolderName);
         },
         filename: (req, file, cb) => {
             return cb(
