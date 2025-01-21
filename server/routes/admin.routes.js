cb72d68adae68f58997b85821223c2135956473a
Arif <webgrity161@gmail.com>
2025-01-21 19:03:56 +0530

    fix middleware for authentication

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const adminRoute = express.Router();
 const { AdminLogin, AdminLogout } = require("../controllers/auth/admin.controller");
-const validAdminVerify = require("../middleware/verify.auth");
 const { AddAuthorController, GetAllAuthorsController, GetSpecificAuthor, DeleteAuthor, UpdateAuthorsController } = require("../controllers/admin/author.controller");
 const uploadMulter = require("../utils/multer");
-const { AddCategoryController, AllCategoryController,DeleteCategoryController } = require("../controllers/admin/category.controller");
+const { AddCategoryController, AllCategoryController, DeleteCategoryController } = require("../controllers/admin/category.controller");
+const AuthenticateJWTtoken = require("../middleware/verify.auth");
+const AuthorizedRole = require("../middleware/roleVerify");
 
 
 
-adminRoute.get("/", validAdminVerify, (req, res) => {
+
+// auth
+adminRoute.post("/login", AdminLogin);
+adminRoute.post("/logout", AdminLogout);
+adminRoute.use(AuthenticateJWTtoken)
+
+adminRoute.get("/", (req, res) => {
     res.send("Working");
 })
 
@@ -27,13 +34,10 @@ adminRoute.delete('/authors/:authorid', DeleteAuthor);
 
 
 // add category
-adminRoute.get("/category", AllCategoryController);
+adminRoute.get("/category", AuthorizedRole(['admins']), AllCategoryController);
 adminRoute.post("/category/add", AddCategoryController);
-adminRoute.delete("/category/:cat_ID",DeleteCategoryController)
+adminRoute.delete("/category/:cat_ID", DeleteCategoryController)
 
 
-// auth
-adminRoute.post("/login", AdminLogin);
-adminRoute.post("/logout", AdminLogout);
 
 module.exports = adminRoute;
