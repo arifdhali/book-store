c2e378d8be56ac15ed669e718f264d451b8c3e20
Arif <webgrity161@gmail.com>
2025-02-26 18:38:50 +0530

    fix categories

--- server/routes/author.protected.routes.js
+++ server/routes/author.protected.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const author_protected_routes = express.Router();
 const HomepageController = require("../controllers/author/Home.Controller");
 const uploadMulter = require("../utils/multer");
-const { AddBookController, BookListController, EditBookController, GetSingleBookController, DeleteBookController } = require("../controllers/author/Book.controller");
+const { AddBookController, BookListController, EditBookController, GetSingleBookController, DeleteBookController, CategoriesController } = require("../controllers/author/Book.controller");
 const { AddCouponController, GetAllCoupons, DeleteCouponsController } = require("../controllers/author/Coupon.controller");
 const { GetSubscription } = require("../controllers/author/Subscription.controller");
 const { GetOrderContoller } = require("../controllers/author/Order.controller");
@@ -14,6 +14,7 @@ const uploadAuthor = uploadMulter("author")
 author_protected_routes.get("/", HomepageController)
 author_protected_routes.post("/book/add", uploadBook.single("thumbnail"), AddBookController);
 author_protected_routes.get("/book/list", BookListController);
+author_protected_routes.get("/book/categories", CategoriesController);
 author_protected_routes.get("/book/:book_id", GetSingleBookController);
 author_protected_routes.patch("/book/:book_id", uploadBook.single("thumbnail"), EditBookController);
 author_protected_routes.delete("/book/:book_id", DeleteBookController)
